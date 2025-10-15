bc11cb4cd07ac9df4b3e105b159ddef5f6e2a060
24100635LopezYepez <lopezyepeznestor@gmail.com>
2025-10-14 21:54:42 -0500

    Update Program.cs completado

--- TercerSemestre/Parcial_2/TEC_04b_Palindromo/Program.cs
+++ TercerSemestre/Parcial_2/TEC_04b_Palindromo/Program.cs
@@ -4,14 +4,35 @@
     {
         static void Main(string[] args)
         {
-            string strCadena = "";
-            Console.WriteLine("Hello, World!");
+            string strCadena = "", strCadenaInv = "";
+            char charCaracter = ' ', charPalindromo = ' ';
+
+            Console.WriteLine(" - - - - - - - - - - - - PALINDROMO E INVERTIDA - - - - - - - - - - - - \n");
+            Console.Write("Introduce la palabra a invertir (y checar su palindromo): ");
             strCadena = Console.ReadLine();
-            Pila<string> stkPila = new Pila<string>(strCadena.Length);
-            for (int i = 0; i <= strCadena.Length; i++) 
+
+            Console.WriteLine("Palabra: " + strCadena + " - Núm. de Caractéres: " + strCadena.Length);
+
+            Pila<char> stkPila = new Pila<char>(strCadena.Length);
+            for (int i = 0; i < stkPila.Max; i++) 
             {
-                
+                charCaracter = strCadena[i];
+                stkPila.Push(charCaracter);
             }
+
+            while(stkPila.Tope > 0) 
+            {
+                charPalindromo = stkPila.Pop();
+                strCadenaInv = strCadenaInv + charPalindromo;
+            }
+            Console.WriteLine("\n Palabra Invertida: " + strCadenaInv);
+
+            if (String.Equals(strCadena, strCadenaInv, StringComparison.OrdinalIgnoreCase))
+            { Console.WriteLine(strCadena + " es palindromo"); }
+            else { Console.WriteLine(strCadena + " no es palindromo"); }
+            
+            Console.ReadKey();
         }
     }
 }
+
