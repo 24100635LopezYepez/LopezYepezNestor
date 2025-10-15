c9da205dc9374eadb800864e4d82decb268fab79
24100635LopezYepez <lopezyepeznestor@gmail.com>
2025-10-15 01:24:16 -0500

    Add files via upload

---
+++ TercerSemestre/Parcial_2/TEC_04a_Sintaxis/Pila.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEC_04a_Sintaxis
+{
+    internal class Pila<Tipo>
+    {
+        private int intMax;
+        private int intTope;
+        private Tipo[] Arreglo;
+
+        public Pila(int intTamanio)
+        {
+            intTope = 0;
+            intMax = intTamanio;
+            Arreglo = new Tipo[intMax];
+        }
+
+        public int Max
+        {
+            get { return (intMax); }
+            private set { intMax = value; }
+        }
+
+        public int Tope
+        {
+            get { return (intTope); }
+            private set { intTope = value; }
+        }
+
+
+        //Metodo de determinar pila llena.
+        private bool EstaLlena()
+        {
+            if (intTope == intMax) { return (true); }
+            else { return (false); }
+        }
+
+        //Método para determinar pila vacía.
+        private bool EstaVacia()
+        {
+            if (intTope <= 0) { return (true); }
+            else { return (false); }
+        }
+
+        //Método para insertar un dato en la pila.
+        public bool Push(Tipo tipoDato)
+        {
+            if (!EstaLlena())
+            {
+                Arreglo[intTope] = tipoDato;
+                intTope++;
+                return (true);
+            }
+            else return (false);
+        }
+
+        //Método para eliminar el ultimo elemento agregado.
+        public Tipo Pop()
+        {
+            if (!EstaVacia())
+            {
+                intTope--;
+                return (Arreglo[intTope]); // Devolver valor correcto.
+            }
+            else
+            {
+                throw new Exception("La pila está vacía"); // Devuelve un "error".
+            }
+        }
+
+        //Mostrar datos
+        public string Mostrar()
+        {
+            string strResultado = "";
+            if (!EstaVacia())
+            {
+                for (int i = 0; i <= intTope; i++)
+                {
+                    strResultado = strResultado + "[" + i.ToString() + "] -> " + Arreglo[i].ToString();
+                    strResultado = strResultado + "\n Top = " + intTope.ToString();
+                    strResultado = strResultado + "\n Max = " + intMax.ToString();
+                }
+                return (strResultado);
+            }
+            else
+            {
+                strResultado += "\n\n Pila Vacía!";
+                return (strResultado);
+            }
+
+        }
+
+        public bool Vacia
+        {
+            get
+            {
+                if (intTope < 0)
+                { return (true); }
+                else
+                { return (false); }
+            }
+        }
+
+        public bool Buscar(Tipo busqueda)
+        {
+            bool blnEncontrado = false;
+            if (EstaVacia()) 
+            { 
+                return (false); 
+            }
+            else
+            {
+                for (int i = 0; i <= intTope; i++)
+                {
+                    if (Arreglo[i].Equals(busqueda)) { blnEncontrado = true; }
+                    else { blnEncontrado = false; }
+                }
+            }
+            return (blnEncontrado);
+        }
+
+        public void Vaciar()
+        {
+            intTope = 0;
+            intMax = 0;
+            Arreglo = null;
+        }
+
+        ~Pila()
+        {
+            Vaciar();
+        }
+    }
+
+}
---
+++ TercerSemestre/Parcial_2/TEC_04a_Sintaxis/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace TEC_04a_Sintaxis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alta y asignacion de valores nulos
+            string strCadena = "";
+            bool blnValido = false, blnCorrer = false;
+            char chrUltimoAbierto = ' ';
+            do
+            {
+                Console.WriteLine(" - - - - - - - - - - - - SINTAXIS - - - - - - - - - - - - \n");
+                Console.Write("Introduce la operación a comprobar su validez: ");
+                strCadena = Console.ReadLine();
+
+                if (strCadena.Contains('(') || strCadena.Contains('[') || strCadena.Contains('{') || strCadena.Contains(')') || strCadena.Contains(']') || strCadena.Contains('}'))
+                {
+                    Pila<char> stkParentesis = new Pila<char>(strCadena.Length);
+                    foreach (char charCaracter in strCadena)
+                    {
+                        if (charCaracter == '{' || charCaracter == '[' || charCaracter == '(')
+                        {
+                            stkParentesis.Push(charCaracter);
+                        }
+                        else if (charCaracter == '}' || charCaracter == ']' || charCaracter == ')')
+                        {
+                            if (!stkParentesis.Vacia)
+                            {
+                                blnValido = true;
+                            }
+
+                            chrUltimoAbierto = stkParentesis.Pop();
+                            if (Coinciden(chrUltimoAbierto, charCaracter))
+                            {
+                                blnValido = true;
+                            }
+                        }
+                    }
+
+                    if (stkParentesis.Vacia)
+                    {
+                        blnValido = true;
+                    }
+                }
+                else
+                {
+                    blnValido = true;
+                }
+                Console.WriteLine("\n - - - - - - VERIFICACIÓN - - - - - - -");
+                if (blnValido) 
+                { 
+                    Console.WriteLine("\n" + strCadena + " es valido."); 
+                }
+                else
+                {
+                    Console.WriteLine("\n" + strCadena + " NO es valido.");
+                }
+
+                Console.WriteLine("\n - - - - - - - - - - - - - - - - - - - - - \n Teclea 's' para salir del programa. ");
+                string strConfirmar = Console.ReadLine();
+                if (string.Equals(strConfirmar, "s") || string.Equals(strConfirmar, "S"))
+                {
+                    blnCorrer = false;
+                }
+                else
+                {
+                    blnCorrer = true;
+                }
+            } while (blnCorrer == true);
+        }
+        private static bool Coinciden(char chrAbierto, char chrCerrado)
+        {
+            return (chrAbierto == '(' && chrCerrado == ')') ||
+                   (chrAbierto == '[' && chrCerrado == ']') ||
+                   (chrAbierto == '{' && chrCerrado == '}');
+        }
+
+    }
+}
